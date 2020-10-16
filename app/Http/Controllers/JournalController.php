b270714c2aa76b91ebc15cfd6dfb7c35eb9d27f7
Alejandro Romero Herrera <alromh87@gmail.com>
2020-10-16 22:42:06 +0300

    Revert "[FIX] Stored-XSS using `htmlentities()`"
    This reverts commit 3871537f759e2e971d252c1e6187c49f2b11e7db.

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -160,7 +160,7 @@ class JournalController extends Controller
 
         $entry = new Entry;
         $entry->account_id = $request->user()->account_id;
-        $entry->post = htmlentities($request->input('entry'), ENT_QUOTES, 'utf-8');
+        $entry->post = $request->input('entry');
 
         if ($request->input('title') != '') {
             $entry->title = $request->input('title');
@@ -206,7 +206,7 @@ class JournalController extends Controller
                 ->withErrors($validator);
         }
 
-        $entry->post = htmlentities($request->input('entry'), ENT_QUOTES, 'utf-8');
+        $entry->post = $request->input('entry');
 
         if ($request->input('title') != '') {
             $entry->title = $request->input('title');

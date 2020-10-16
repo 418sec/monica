5f1d29be8962342479040e3e86fc82302fe66c4e
Alejandro Romero Herrera <alromh87@gmail.com>
2020-10-16 22:51:38 +0300

    Revert "Filter also when no user"
    This reverts commit c9b516cfbd63fc5073c41b81a0ab4cc15c414f05.

--- app/Helpers/AuditLogHelper.php
+++ app/Helpers/AuditLogHelper.php
@@ -39,7 +39,7 @@ class AuditLogHelper
                     $description = trans('logs.settings_log_'.$log->action.'_with_name', ['name' => $log->object->{'contact_name'}]);
                 }
             } else {
-                $description = trans('logs.settings_log_'.$log->action, ['name' => htmlentities($log->object->{'name'}, ENT_QUOTES, 'utf-8')]);
+                $description = trans('logs.settings_log_'.$log->action, ['name' => $log->object->{'name'}]);
             }
 
             $logsCollection->push([
